df85bb83fac497e36fa85ff3c64c5ae25270e1cc
Fadhil <111748979+DumbiFadhil@users.noreply.github.com>
2024-01-28 20:25:09 +0700

    fix: proses_tambah_ruangan.php error

--- util/proses_tambah_ruangan.php
+++ util/proses_tambah_ruangan.php
@@ -1,20 +1,22 @@
 <?php
-include ("connection.php");
+include("connection.php");
 if (isset($_POST['tambah_button'])) {
+  try {
     $id_kelas = $_POST['id_kelas'];
     $kapasitas = $_POST['kapasitas'];
     $jenis_kelas = $_POST['jenis_kelas'];
     $lokasi_kelas = $_POST['lokasi_kelas'];
     $fasilitas_penunjang = $_POST['fasilitas_penunjang'];
 
-
     // Update the database
-    $sql = "INSERT INTO ruangan (id_ruangan, kapasitas, jenis_ruangan, lokasi, fasilitas)
-    VALUES ('$id_kelas', '$kapasitas', '$jenis_kelas', '$lokasi_kelas', '$fasilitas_penunjang')";
+    $sql = "insert into ruangan (id_ruangan, kapasitas, jenis_ruangan, lokasi, fasilitas) VALUES ('$id_kelas', '$kapasitas', '$jenis_kelas', '$lokasi_kelas', '$fasilitas_penunjang')";
     mysqli_query($conn, $sql);
 
     // Close the database connection
     $conn->close();
-
+  } catch (\Throwable $th) {
+    echo "Error: " . $th->getMessage;
+  } finally {
+    header("Location: ../page/kelas_admin.php");
+  }
 }
-?>
