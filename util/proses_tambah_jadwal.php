a8d536a61517afbc5adce517eef7b3e5835b1d93
Fadhil <111748979+DumbiFadhil@users.noreply.github.com>
2024-01-28 23:48:03 +0700

    fix: fixed proses_tambah_jadwal.php

--- util/proses_tambah_jadwal.php
+++ util/proses_tambah_jadwal.php
@@ -2,18 +2,18 @@
 include("connection.php");
 if (isset($_POST['tambah_button'])) {
   try {
-    $id_ruangan = $_POST['id_ruangan']; -
-    $hari = $_POST['hari'];-
+    $id_ruangan = $_POST['id_ruangan'];
+    $hari = $_POST['hari'];
+    $tanggal = $_POST['tanggal'];
     $waktu_mulai = $_POST['waktu_mulai'];
     $waktu_selesai = $_POST['waktu_selesai'];
-    $id_matkul = $_POST['id_matkul'];-
-    $NIP = $_POST['NIP'];-
+    $id_matkul = $_POST['id_matkul'];
+    $NIP = $_POST['NIP'];
     $keterangan = $_POST['keterangan'];
     $status = $_POST['status'];
 
     // Update the database
-    $sql = "insert into jadwal (id_ruangan, id_matkul, NIP, hari, waktu_mulai, waktu_selesai, keterangan, status) 
-    VALUES ('$id_ruangan', '$id_matkul', '$NIP', '$hari', '$waktu_mulai', '$waktu_selesai', '$keterangan', '$status')";
+    $sql = "insert into jadwal (id_ruangan, id_matkul, NIP, hari, tanggal, waktu_mulai, waktu_selesai, keterangan, status) VALUES ('$id_ruangan', '$id_matkul', '$NIP', '$hari', '$tanggal', '$waktu_mulai', '$waktu_selesai', '$keterangan', '$status')";
     mysqli_query($conn, $sql);
 
     // Close the database connection
